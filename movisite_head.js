c730c7221bde13d607421af8703a76b16278ee9e
bloggermasterhub <79012517+bloggermasterhub@users.noreply.github.com>
2021-05-31 18:42:32 +0800

    Create movisite_head.js

---
+++ movisite_head.js
@@ -0,0 +1,7 @@
+// Global variables. "Available for Edit"
+var monthFormat = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+    noThumbnail = "https://4.bp.blogspot.com/-O3EpVMWcoKw/WxY6-6I4--I/AAAAAAAAB2s/KzC0FqUQtkMdw7VzT6oOR_8vbZO6EJc-ACK4BGAYYCw/s1600/nth.png",
+    postPerPage = 7,
+    fixedSidebar = true,
+    commentsSystem = "blogger",
+    disqusShortname = "soratemplates";
