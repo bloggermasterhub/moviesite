eba09dcf97894ecd88bd29fb0f2f491bdd9287a1
bloggermasterhub <79012517+bloggermasterhub@users.noreply.github.com>
2021-06-08 20:03:47 +0800

    Create adsterra90x728.js

---
+++ adsterra90x728.js
@@ -0,0 +1,9 @@
+>
+	atOptions = {
+		'key' : 'd4286f192d85246b86ea4e6f3fa3bcee',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://nicknameuntie.com/d4286f192d85246b86ea4e6f3fa3bcee/invoke.js"></scr' + 'ipt>');
